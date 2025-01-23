19962fffb4cd9ef75b4845d40fbd676af5353344
Manojit Saha <118467865+MANOJIT2943P@users.noreply.github.com>
2025-01-23 23:32:01 +0530

    removing poster feature

--- app.py
+++ app.py
@@ -11,14 +11,7 @@ movies=pd.DataFrame(movies)
 with gzip.open('similarity.pkl.gz','rb') as f:
     similarity=pickle.load(f)
 
-#function to fetch poster
-def fetch_poster(movie_id):
-     url = "https://api.themoviedb.org/3/movie/{}?api_key=fe6aacca86b69ab259c31a74a20127b6&language=en-US".format(movie_id)
-     data=requests.get(url)
-     data=data.json()
-     poster_path = data['poster_path']
-     full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
-     return full_path
+
 
 #function to recommend movies
 def recommend(mov):
@@ -27,13 +20,11 @@ def recommend(mov):
     movie_list=sorted(list(enumerate(dis)),reverse=True,key=lambda x: x[1])[1:6]
 
     rec_mov=[]
-    rec_mov_poster=[]
     for i in movie_list:
         movie_id=movies.iloc[i[0]].id
         rec_mov.append(movies.iloc[movie_id].title)
-        rec_mov_poster.append(fetch_poster(movie_id))
 
-    return rec_mov,rec_mov_poster
+    return rec_mov
 
 
 #User Interface
@@ -50,16 +41,11 @@ if st.button("Recommend"):
 
     with col1:
         st.text(rec[0])
-        st.image(pos[0])
     with col2:
         st.text(rec[1])
-        st.image(pos[1])
     with col3:
         st.text(rec[2])
-        st.image(pos[2])
     with col4:
         st.text(rec[3])
-        st.image(pos[3])
     with col5:
         st.text(rec[4])
-        st.image(pos[4])
